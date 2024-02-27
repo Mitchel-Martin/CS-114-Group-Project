342acb3417b0d685337a3a0b143e70face4130f8
Mitchel-Martin <mitchm03@icloud.com>
2024-02-27 13:43:31 -0500

    1.0.2 set up

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,23 @@
         private void InitializeComponent()
         {
             lblCreditCardInfo = new Label();
-            textBox1 = new TextBox();
+            txtCardNumber = new TextBox();
             button1 = new Button();
             lblCardNumber = new Label();
             lblCCV = new Label();
             lblExpiration = new Label();
-            textBox2 = new TextBox();
+            txtCCV = new TextBox();
+            cboMonth = new ComboBox();
+            cboDay = new ComboBox();
+            cboYear = new ComboBox();
+            lblPersonalInfo = new Label();
+            lblName = new Label();
+            label2 = new Label();
+            lblSSN = new Label();
+            label4 = new Label();
+            textBox1 = new TextBox();
+            txtSSN = new TextBox();
+            textBox3 = new TextBox();
             comboBox1 = new ComboBox();
             SuspendLayout();
             // 
@@ -47,16 +58,16 @@
             lblCreditCardInfo.TabIndex = 0;
             lblCreditCardInfo.Text = "Enter Your Credit Card Information";
             // 
-            // textBox1
+            // txtCardNumber
             // 
-            textBox1.Location = new Point(653, 133);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(200, 39);
-            textBox1.TabIndex = 1;
+            txtCardNumber.Location = new Point(653, 133);
+            txtCardNumber.Name = "txtCardNumber";
+            txtCardNumber.Size = new Size(375, 39);
+            txtCardNumber.TabIndex = 1;
             // 
             // button1
             // 
-            button1.Location = new Point(616, 606);
+            button1.Location = new Point(600, 1173);
             button1.Name = "button1";
             button1.Size = new Size(150, 46);
             button1.TabIndex = 2;
@@ -75,7 +86,7 @@
             // lblCCV
             // 
             lblCCV.AutoSize = true;
-            lblCCV.Location = new Point(241, 239);
+            lblCCV.Location = new Point(241, 197);
             lblCCV.Name = "lblCCV";
             lblCCV.Size = new Size(271, 32);
             lblCCV.TabIndex = 4;
@@ -84,39 +95,140 @@
             // lblExpiration
             // 
             lblExpiration.AutoSize = true;
-            lblExpiration.Location = new Point(241, 339);
+            lblExpiration.Location = new Point(241, 259);
             lblExpiration.Name = "lblExpiration";
             lblExpiration.Size = new Size(282, 32);
             lblExpiration.TabIndex = 5;
             lblExpiration.Text = "Enter the Expiration date:";
             // 
-            // textBox2
+            // txtCCV
             // 
-            textBox2.Location = new Point(653, 236);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(200, 39);
-            textBox2.TabIndex = 6;
+            txtCCV.Location = new Point(653, 194);
+            txtCCV.Name = "txtCCV";
+            txtCCV.Size = new Size(129, 39);
+            txtCCV.TabIndex = 6;
+            // 
+            // cboMonth
+            // 
+            cboMonth.FormattingEnabled = true;
+            cboMonth.Location = new Point(810, 256);
+            cboMonth.Name = "cboMonth";
+            cboMonth.Size = new Size(218, 40);
+            cboMonth.TabIndex = 7;
+            // 
+            // cboDay
+            // 
+            cboDay.FormattingEnabled = true;
+            cboDay.Location = new Point(1057, 256);
+            cboDay.Name = "cboDay";
+            cboDay.Size = new Size(125, 40);
+            cboDay.TabIndex = 8;
+            // 
+            // cboYear
+            // 
+            cboYear.FormattingEnabled = true;
+            cboYear.Location = new Point(653, 256);
+            cboYear.Name = "cboYear";
+            cboYear.Size = new Size(129, 40);
+            cboYear.TabIndex = 9;
+            // 
+            // lblPersonalInfo
+            // 
+            lblPersonalInfo.AutoSize = true;
+            lblPersonalInfo.Location = new Point(498, 344);
+            lblPersonalInfo.Name = "lblPersonalInfo";
+            lblPersonalInfo.Size = new Size(351, 32);
+            lblPersonalInfo.TabIndex = 10;
+            lblPersonalInfo.Text = "Enter Your Personal Information";
+            // 
+            // lblName
+            // 
+            lblName.AutoSize = true;
+            lblName.Location = new Point(241, 420);
+            lblName.Name = "lblName";
+            lblName.Size = new Size(295, 32);
+            lblName.TabIndex = 11;
+            lblName.Text = "Enter your full legal name:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(241, 562);
+            label2.Name = "label2";
+            label2.Size = new Size(300, 32);
+            label2.TabIndex = 12;
+            label2.Text = "Enter your current address:";
+            // 
+            // lblSSN
+            // 
+            lblSSN.AutoSize = true;
+            lblSSN.Location = new Point(241, 487);
+            lblSSN.Name = "lblSSN";
+            lblSSN.Size = new Size(385, 32);
+            lblSSN.TabIndex = 13;
+            lblSSN.Text = "Enter your Social Security Number:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(241, 630);
+            label4.Name = "label4";
+            label4.Size = new Size(685, 32);
+            label4.TabIndex = 14;
+            label4.Text = "Enter any other Personal information you are willing to donate:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(653, 417);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(529, 39);
+            textBox1.TabIndex = 15;
+            // 
+            // txtSSN
+            // 
+            txtSSN.Location = new Point(653, 484);
+            txtSSN.Name = "txtSSN";
+            txtSSN.Size = new Size(529, 39);
+            txtSSN.TabIndex = 16;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(810, 559);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(372, 39);
+            textBox3.TabIndex = 17;
             // 
             // comboBox1
             // 
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(653, 336);
+            comboBox1.Location = new Point(653, 559);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(242, 40);
-            comboBox1.TabIndex = 7;
+            comboBox1.Size = new Size(129, 40);
+            comboBox1.TabIndex = 18;
             // 
             // frmHealthWellness
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1362, 919);
+            ClientSize = new Size(1362, 1247);
             Controls.Add(comboBox1);
-            Controls.Add(textBox2);
+            Controls.Add(textBox3);
+            Controls.Add(txtSSN);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(lblSSN);
+            Controls.Add(label2);
+            Controls.Add(lblName);
+            Controls.Add(lblPersonalInfo);
+            Controls.Add(cboYear);
+            Controls.Add(cboDay);
+            Controls.Add(cboMonth);
+            Controls.Add(txtCCV);
             Controls.Add(lblExpiration);
             Controls.Add(lblCCV);
             Controls.Add(lblCardNumber);
             Controls.Add(button1);
-            Controls.Add(textBox1);
+            Controls.Add(txtCardNumber);
             Controls.Add(lblCreditCardInfo);
             Name = "frmHealthWellness";
             Text = "Health and Wellness App";
@@ -127,12 +239,23 @@
         #endregion
 
         private Label lblCreditCardInfo;
-        private TextBox textBox1;
+        private TextBox txtCardNumber;
         private Button button1;
         private Label lblCardNumber;
         private Label lblCCV;
         private Label lblExpiration;
-        private TextBox textBox2;
+        private TextBox txtCCV;
+        private ComboBox cboMonth;
+        private ComboBox cboDay;
+        private ComboBox cboYear;
+        private Label lblPersonalInfo;
+        private Label lblName;
+        private Label label2;
+        private Label lblSSN;
+        private Label label4;
+        private TextBox textBox1;
+        private TextBox txtSSN;
+        private TextBox textBox3;
         private ComboBox comboBox1;
     }
 }
